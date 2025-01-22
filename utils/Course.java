189243c7e5bf770b5189a14187b6da3d37854e30
ChanpanhaCheng <chanpanhacheng@gmail.com>
2025-01-22 22:10:58 +0700

    fixing construtor

--- utils/Course.java
+++ utils/Course.java
@@ -13,6 +13,14 @@ public class Course {
         this.payment = payment;
         this.description = description;
     }
+    public Course(int id,String name,String level,float payment) {
+        this.id = id;
+        this.name = name;
+        this.level = level;
+        this.payment = payment;
+        this.description = "null";
+
+    }
 
     public int getId() {
         return id;
