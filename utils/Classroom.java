27bec8d59acaf1871f5f1aac0cecb5d4e3063acc
Chhun Sivheng <sivhengchhun06@gmail.com>
2025-01-23 09:02:59 +0700

    Update Classroom

--- utils/Classroom.java
+++ utils/Classroom.java
@@ -1,41 +1,17 @@
 package utils;
+
+import java.util.*;
+
 public class Classroom {
+    private int idClass;
+    private Teacher teacher = new Teacher();
+    private Course course;
+    private ArrayList<Student> student = new ArrayList<Student>();
 
-    private String idClass;
-    private String nameClass;
-    private String teacher;
-    private int capacity;
-
-    public Classroom(String idClass, String className, String teacher, int capacity){
-        this.idClass = className;
-        this.nameClass = className;
-        this.teacher = teacher;
-        this.capacity = capacity;
-    }
-
-    public String getClassId(){
-        return idClass;
-    }
-    public String getClassName(){
-        return nameClass;
-    }
-    public String getTeacher(){
-        return teacher;
-    }
-    public int getCapacity(){
-        return capacity;
-    }
-
-    public void setClassId(String idClass){
+    public Classroom(int idClass, Course course, Teacher teacher, ArrayList<Student> student ){
         this.idClass = idClass;
-    }
-    public void setClassName(String nameClass){
-        this.nameClass = nameClass;
-    }
-    public void setTeacher(String teacher){
         this.teacher = teacher;
-    }
-    public void setCapacity(int capacity){
-        this.capacity = capacity;
+        this.course = course;
+        this.student = student;
     }
 }
