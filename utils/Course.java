b6e69d8a826c9c18ed42284325a9297cf7b91666
vathanakphy <vathanakphy@gmail.com>
2025-01-30 10:12:08 +0700

    update modifier

--- utils/Course.java
+++ utils/Course.java
@@ -1,23 +1,23 @@
 package utils;
 public class Course {
-    private int id;
-    private String name;
-    private String level;
-    private float payment;
-    private String description;
+    public int id;
+    public String name;
+    public String level;
+    public float fee;
+    public String description;
 
-    public Course(int id, String name, String level, float payment, String description) {
+    public Course(int id, String name, String level, float fee, String description) {
         this.id = id;
         this.name = name;
         this.level = level;
-        this.payment = payment;
+        this.fee = fee;
         this.description = description;
     }
-    public Course(int id,String name,String level,float payment) {
+    public Course(int id,String name,String level,float fee) {
         this.id = id;
         this.name = name;
         this.level = level;
-        this.payment = payment;
+        this.fee = fee;
         this.description = "null";
 
     }
