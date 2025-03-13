75e24845103b3f3687fe85e49d362a689c6f4f59
ChanpanhaCheng <chanpanhacheng@gmail.com>
2025-03-13 15:15:30 +0700

    temp save

---
+++ gui/tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+frame.add(sidebar, BorderLayout.WEST);
+        frame.add(topBar, BorderLayout.NORTH);
+        frame.add(mainContent, BorderLayout.CENTER);
---
+++ progress/testgui.java
@@ -0,0 +1,14 @@
+package progress;
+
+import gui.framBackground;
+import javax.swing.JFrame;
+
+public class testgui {
+    public static void main(String[] args) {
+        
+        framBackground fb ;
+        fb.frame = new JFrame("Student Dashboard");
+       
+    
+    }
+}
